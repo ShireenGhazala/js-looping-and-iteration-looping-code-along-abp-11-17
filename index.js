b2a423f873e0490fdec2fd6aed9af6cdce8d3aa7
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-14 21:56:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 function printBadges(arr){
    arr=["Lisa", "Kaitlin", "Jan"];
   for (i = 0; i < arr.length; i++){
-    arr[i];
    console.log(`Welcome ${arr[i]}! You are employee no: ${i+1}.`)
   }
 
