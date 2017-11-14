2e05ce7abf1d1a6fdfcdd58fe9ffd00d6bc2515d
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-14 21:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const arr=[];
+const arr=["Lisa", "Kaitlin", "Jan"];
 function printBadges(arr){
   for (i=0; i<arr.length ; i++){
     console.log( `employee ${i} : ${arr[i]}`);
-    return 
+    return
   }
 }
